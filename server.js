28a3b7628f10c378938cb3104d5e10960983b357
emonahmed123 <imonshomon@gmail.com>
2022-09-10 22:21:03 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+// require("dotenv").config()
+// const DBConnect = require("./utils/dbConnect");
+
+const app = require("./app");
+
+// database connection
+mongoose.connect(process.env.LOCAL_DATABASE).then(()=>{
+    console.log(`database coonction is successfull`)
+})
+
+// server
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`.yellow.bold);
+});
